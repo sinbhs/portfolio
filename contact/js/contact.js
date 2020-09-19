0a5e2e601a705c88b05800245737b509e2c9e7d2
sinbhs <sinbhs@naver.com>
2020-09-19 18:24:44 +0900

    add contact page file

---
+++ contact/js/contact.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    // mobile, tablet Menu Overlay
+    $(".overlay").hide();
+    $('.menu_icon').click(function(){
+        $(this).toggleClass('active')
+        const menu = $('.menu_icon').hasClass('active');
+        if(menu) {
+            $('nav h2').css('opacity','0');
+            $('header').css({'background':'none','border-bottom':'#004be2'})
+            $('.overlay').slideDown();
+        } else {
+            $('nav h2').css('opacity','1');
+            $('header').css({'background':'#f9f8fa94','border-bottom':'1px solid #ddd'})
+            $('.overlay').slideUp();
+        }
+    });
+    // back to top Btn
+    $(".return_top").click(function(){
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+    //scroll nav event
+    $(window).scroll(function(){
+        var scroll = $(this).scrollTop();
+        if (scroll > 150) {
+            $('header').addClass('scroll-down');
+            $('.menu_icon').on('click',function(){
+                $('header').removeClass('scroll-down');
+            });
+        } else {
+            $('header').removeClass('scroll-down');
+        }
+    });
+});
